135bd52b170bf716fb3ad4f6356af69d0428c716
张云飞 <zhangyunfei@okjiaoyu.cn>
2017-09-05 13:27:40 +0800

    xiufu klogin secret

--- config/services.php
+++ config/services.php
@@ -37,13 +37,13 @@ return [
 
     'github' => [
         'client_id' => env('GITHUB_KEY'),
-        'client_secret' => env('GITHUB_KEY'),
+        'client_secret' => env('GITHUB_SECRET'),
         'redirect' => env('GITHUB_REDIRECT_URI'),
     ],
 
     'google' => [
         'client_id' => env('GOOGLE_KEY'),
-        'client_secret' => env('GOOGLE_KEY'),
+        'client_secret' => env('GOOGLE_SECRET'),
         'redirect' => env('GOOGLE_REDIRECT_URI'),
     ],
 

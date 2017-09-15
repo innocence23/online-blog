47e5da6958d31fea1a1cbd0ab806e061d02cdb90
张云飞 <zhangyunfei@okjiaoyu.cn>
2017-09-15 09:34:54 +0800

    屏蔽微信 个人用户没权限用自定义功能

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
+	protected $fillable = ['name', 'created_by', 'updated_by'];
+	// protected $guarded = [];
     //
     static public function tagList()
     {

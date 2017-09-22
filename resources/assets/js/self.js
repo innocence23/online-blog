c8e87f7e1111ffd27acceac8b697b495b9a037b6
张云飞 <zhangyunfei@okjiaoyu.cn>
2017-09-22 15:12:41 +0800

    整理合并前天页面样式和js

--- resources/assets/js/self.js
+++ resources/assets/js/self.js
@@ -1,4 +1,4 @@
 $(function () {
-    $('.scroll-top').click(function(){$('html,body').animate({scrollTop: '0px'}, 800);});
-    $('.scroll-bottom').click(function(){$('html,body').animate({scrollTop:$('.copyright').offset().top}, 800);});
+    $('.scroll-top').click(function(){$('html,body').animate({scrollTop: 0}, 800, 'swing')});
+    $('.scroll-bottom').click(function(){$('html,body').animate({scrollTop:$('#bottom').offset().top}, 800, 'swing')});
 });
--- resources/views/layouts/partials/js.blade.php
+++ resources/views/layouts/partials/js.blade.php
@@ -1,2 +1,3 @@
+<div id="bottom"></div>
 <script src="{{ mix('/assets/js/app.js') }}" type="text/javascript"></script>
 @yield('js')
